444e30fe35a0a655d300ba1386dd05bcd5450f47
Joe Amenta <airbreather@linux.com>
2016-01-23 19:24:53 -0500

    Limiting squid spawn rate. Apparently it's a problem.  Maybe this solves it.

--- src/main/java/airbreather/mods/cacophonyjanitor/CacophonyJanitorConstants.java
+++ src/main/java/airbreather/mods/cacophonyjanitor/CacophonyJanitorConstants.java
@@ -3,18 +3,21 @@ package airbreather.mods.cacophonyjanitor;
 // Some constants used throughout the mod.
 final class CacophonyJanitorConstants
 {
-    // ID of the mod.
-    public static final String modID = "cacophonyjanitor";
+	// ID of the mod.
+	public static final String modID = "cacophonyjanitor";
 
-    // Name of the mod.
-    public static final String modName = "Cacophony Janitor";
+	// Name of the mod.
+	public static final String modName = "Cacophony Janitor";
 
-    // Refers to the quest reward proxy items.
-    public static final String violenceLunchProxyItemID = "violenceLunchProxy";
-    public static final String indolenceLunchProxyItemID = "indolenceLunchProxy";
-    public static final String passionLunchProxyItemID = "passionLunchProxy";
-    public static final String hungerLunchProxyItemID = "hungerLunchProxy";
-    public static final String doggieBagProxyItemID = "doggieBagProxy";
-    public static final String rivalryLunchProxyItemID = "rivalryLunchProxy";
-    public static final String smokingBoxProxyItemID = "smokingBoxProxy";
+	// Refers to the quest reward proxy items.
+	public static final String violenceLunchProxyItemID = "violenceLunchProxy";
+	public static final String indolenceLunchProxyItemID = "indolenceLunchProxy";
+	public static final String passionLunchProxyItemID = "passionLunchProxy";
+	public static final String hungerLunchProxyItemID = "hungerLunchProxy";
+	public static final String doggieBagProxyItemID = "doggieBagProxy";
+	public static final String rivalryLunchProxyItemID = "rivalryLunchProxy";
+	public static final String smokingBoxProxyItemID = "smokingBoxProxy";
+
+	// How many squids to skip spawning before allowing one in, by default.
+	public static final int defaultSkipsBeforeSquidSpawn = 3;
 }
